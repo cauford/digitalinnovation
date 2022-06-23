d091497837fec3458735fc97fac4cd8a2f29ff5b
cauford <ctmsmaster@gmail.com>
2022-06-23 08:05:45 -0300

    created bank class and test to it

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,14 +2,15 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Carlos"
-
     var cpf : String = "123.123.123-12"
     private set
+
+    constructor()
+
+    fun pessoainfo() = "$nome ---> $cpf"
 }
 
 fun main() {
-    val Carlos = Pessoa()
-
-    println(Carlos.nome)
-    println(Carlos.cpf)
+    val carlos = Pessoa()
+    println(carlos.pessoainfo())
 }
